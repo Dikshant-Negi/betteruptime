d47c2708ab9abde46d408f98f144455038644e99
Dikshant-Negi <dikshantnegi92@gmail.com>
2025-11-15 21:23:14 +0530

    added redis operations

--- redis_stream/src/redis_client.rs
+++ redis_stream/src/redis_client.rs
@@ -1,5 +1,7 @@
-use redis::{self, RedisResult, Connection};
-
+use redis::{
+    self, Commands, Connection, RedisResult,
+    streams::{StreamReadOptions, StreamReadReply},
+};
 
 pub struct RedisStream {
     pub conn: Connection,
@@ -7,21 +9,46 @@ pub struct RedisStream {
 
 impl RedisStream {
     pub fn new(url: &str) -> RedisResult<Self> {
-
         let client = redis::Client::open(url)?;
         let conn = client.get_connection()?;
 
         Ok(Self { conn })
     }
 
-    pub fn x_add(&mut self,id:&str,website:&str)->RedisResult<()>{
+    pub fn add_consumer(&mut self) -> RedisResult<()> {
+        let result: RedisResult<()> =
+            self.conn
+                .xgroup_create_mkstream("betteruptime:website", "uptime-checkers", "$");
+
+        match result {
+            Ok(_) => println!("Consumer group created!"),
+            Err(err) => println!("Group already exists: {}", err),
+        }
+
+        Ok(())
+    }
+
+    pub fn x_add(&mut self, id: &str, website: &str) -> RedisResult<()> {
         let payload = serde_json::json!({
             "id": id,
             "website": website
-        });
+        }).to_string();
 
-        self::conn.xadd("betteruptime:website","*",payload);
+        let res :RedisResult<()> = self.conn.xadd("betteruptime:website", "*", &[("payload",payload)]);
 
         Ok(())
+    }
+
+    pub fn x_read_group(&mut self, consumer_name: &str) -> RedisResult<StreamReadReply> {
+        let options = StreamReadOptions::default()
+            .group("uptime-checkers", consumer_name)
+            .count(10)
+            .block(0);
+
+        let reply: StreamReadReply =
+            self.conn
+                .xread_options(&["betteruptime:website"], &[">"], options)?;
+
+        Ok(reply)
     }
 }
